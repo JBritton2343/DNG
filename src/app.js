5fed534f90de98abaa9ceb0ccda6bc554c39b63a
JBritton2343 <jbritton854@gmail.com>
2022-07-25 14:58:19 -0500

    updated with nested for loops

--- src/app.js
+++ src/app.js
@@ -6,31 +6,25 @@ let ext = ['com', 'org', 'gov', 'edu']
 
 window.onload = function() {
   //write your code here
-  function DNG(pronounArr, adjArr, nounArr, extArr) {
-    let randPronoun = genRanNum(pronounArr.length);
-    let myPronoun = pronounArr[randPronoun];
-    console.log(myPronoun);
+  function domainNameGen(pronounArr, adjArr, nounArr, extArr) {
+    let domainName = "";
+    for(let i = 0; i < Array.length; i++){
+      for(let r = 0; r < arr[i].length; r++){
+        for(let p = 0; p < arr[r].length; p++){
+          for(let t = 0; t<arr[p].length; t++ ){
 
-    let randAdj = genRanNum(adjArr.length);
-    let myAdj = adjArr[randAdj];
-    console.log(myAdj);
+          }
+        }
 
-    let randNoun = genRanNum(nounArr.length);
-    let myNoun = nounArr[randNoun];
-    console.log(myNoun);
-
-    let randExt = genRanNum(extArr.length);
-    let myExt = extArr[randExt];
-    console.log(myExt);
+      }
+    }
+    
 
     
     return `${myPronoun}` + `${myAdj}` + `${myNoun}` + `.` + `${myExt}`;
   }
 
   let myDNG = document.getElementById("myDNG");
-  myDNG.innerHTML=DNG(pronoun, adj, noun, ext);
+  myDNG.innerHTML=domainNameGen(pronoun, adj, noun, ext);
   console.log(myDNG);
 }; 
-function genRanNum(max) {
-  return Math.floor(Math.random() * max);
-}
