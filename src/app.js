eeb421b3e2c959b5025d2bdd4d8f947e9b8a073e
JBritton2343 <jbritton854@gmail.com>
2022-07-25 18:25:15 -0500

    updated to nested forloops

--- src/app.js
+++ src/app.js
@@ -1,46 +1,36 @@
 /* eslint-disable */
-let pronoun = ['the', 'our', 'your', 'his', 'her'];
-let adj = ['great', 'galactic', 'massive', 'diabolic'];
-let noun = ['apple', 'bear', 'coyote', 'shark', 'jogger', 'racoon'];
-let ext = ['com', 'org', 'gov', 'edu']
+let pronoun = ['the', 'our', 'your'];
+let adj = ['great', 'galactic', 'diabolic'];
+let noun = ['apple', 'bear', 'coyote'];
+let ext = ['com', 'org', 'edu'];
 
-window.onload = function() {
+
   //write your code here
-  const domainNameGen = () => {
+  const domainNameGen = (pronounArr, adjArr, nounArr, extArr) => {
 
-    let getDomainName = () =>{
-
-      pronoun.forEach(function(pronoun){
-        randomNum(0, pronoun.length);
-        return pronoun;
-      })
-      adj.forEach(function(adj){
-        randomNum(0, adj.length);
-        return adj;
-      })
-      noun.forEach(function(noun){
-        randomNum(0, noun.length);
-        return adj;
-      })
-      ext.forEach(function(ext){
-        randomNum(0, ext.length);
-        return ext;
-      })
-
+      for(let i=0; i<pronounArr.length; i++){
+        for(let j=0; j<adjArr.length; j++){
+          for(let k=0; k<nounArr.length; k++){
+            for(let l=0; l<extArr.length; l++){
+              let string = `${pronounArr[i]}${adjArr[j]}
+              ${nounArr[k]}.${extArr[l]}`
+            }
+          }
+        }
+      }
+      return string;
+    }
       
 
-      }
-    }
+     
+
+      
+    
    
 
-  }    
+     
   let dngList = document.getElementById("myDNG");
   myDNG.innerHTML=domainNameGen(pronoun, adj, noun, ext);
   console.log(dngList);
 
-let randomNum =(min, max)=> {
-  let firstStep = max - min +1;
-  let second= Math.random()+firstStep;
-  let result = Math.floor(second) + min;
-  return result;
-}
+
