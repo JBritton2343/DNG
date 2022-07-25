22e7d654bed3a70b7c0ad8a4538250385efafdae
JBritton2343 <jbritton854@gmail.com>
2022-07-25 17:35:27 -0500

    updated to forEach loops

--- src/app.js
+++ src/app.js
@@ -8,12 +8,39 @@ window.onload = function() {
   //write your code here
   const domainNameGen = () => {
 
-    let domainName = () =>
-    `${prounoun[randomNum(pronoun.length)]}${adj[randomNum(adj.length)]}
-    ${noun[randomNum(noun.length)]}.${ext[randomNum(ext.length)]}`
+    let getDomainName = () =>{
+
+      pronoun.forEach(function(pronoun){
+        randomNum(0, pronoun.length);
+        return pronoun;
+      })
+      adj.forEach(function(adj){
+        randomNum(0, adj.length);
+        return adj;
+      })
+      noun.forEach(function(noun){
+        randomNum(0, noun.length);
+        return adj;
+      })
+      ext.forEach(function(ext){
+        randomNum(0, ext.length);
+        return ext;
+      })
+
+      
+
+      }
+    }
+   
+
   }    
   let dngList = document.getElementById("myDNG");
-  myDNG.innerHTML=domainNameGen();
+  myDNG.innerHTML=domainNameGen(pronoun, adj, noun, ext);
   console.log(dngList);
-}; 
-let randomNum =(max)=> Math.floor(Math.random() * max);
+
+let randomNum =(min, max)=> {
+  let firstStep = max - min +1;
+  let second= Math.random()+firstStep;
+  let result = Math.floor(second) + min;
+  return result;
+}
